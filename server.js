dd7c804796c3280f6a532dede4b76ae17b479555
Leonardo Souza <leonardosouza@TARS.local>
2024-01-24 20:04:11 -0300

    .env.example added

--- server.js
+++ server.js
@@ -29,19 +29,23 @@ const User = connection.promise().query(`
         id INT AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL,
-        password VARCHAR(255) NOT NULL
+        password VARCHAR(255) NOT NULL,
+        name VARCHAR(45) NOT NULL,
+        surname VARCHAR(255) NOT NULL
     )
 `);
 
 app.post("/register", async (req, res) => {
   try {
-    const { username, email, password } = req.body;
+    const { username, email, password, name, surname } = req.body;
     await connection
       .promise()
-      .query("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", [
+      .query("INSERT INTO users (username, email, password, name, surname) VALUES (?, ?, ?, ?, ?)", [
         username,
         email,
         password,
+        name,
+        surname
       ]);
     res.status(201).json({ message: "Usuário registrado com sucesso" });
   } catch (error) {
