ec9951014000ea38d8dd3db1d0ef374e7cf36a9e
Leonardo Souza <leonardosouza@TARS.local>
2024-01-25 14:33:09 -0300

    fix: remoção de variável não utilizada

--- server.js
+++ server.js
@@ -24,23 +24,12 @@ connection.connect((error) => {
   }
 });
 
-const User = connection.promise().query(`
-    CREATE TABLE IF NOT EXISTS users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        name VARCHAR(45) NOT NULL,
-        surname VARCHAR(255) NOT NULL
-    )
-`);
-
 app.post("/register", async (req, res) => {
   try {
     const { username, email, password, name, surname } = req.body;
     await connection
       .promise()
-      .query("INSERT INTO users (username, email, password, name, surname) VALUES (?, ?, ?, ?, ?)", [
+      .query("INSERT INTO users (usersname, email, password, name, surname) VALUES (?, ?, ?, ?, ?)", [
         username,
         email,
         password,
