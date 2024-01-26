9ed557e402b9805ce7e6e593d07f1801b93d7eac
Leonardo Souza <leonardosouza@TARS.local>
2024-01-26 14:14:35 -0300

    chore: validação de nome de usuário e e-mail duplicados

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mysql = require("mysql2");
 const cors = require("cors");
 
 //Variáveis para criptografia
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 const app = express();
@@ -31,16 +31,29 @@ connection.connect((error) => {
 app.post("/register", async (req, res) => {
   try {
     const { username, email, password, name, surname } = req.body;
+    const [userToCheck] = await connection
+      .promise()
+      .query("SELECT COUNT(id) AS userCount FROM USERS WHERE username = ?", [
+        username,
+      ]);
+    const [emailToCheck] = await connection
+      .promise()
+      .query("SELECT COUNT(id) AS emailCount FROM USERS WHERE email = ?", [
+        email,
+      ]);
+    if (userToCheck[0].userCount > 0) {
+      return res.status(422).json({ message: "Usuário já cadastrado" });
+    }
+    if (emailToCheck[0].emailCount > 0) {
+      return res.status(422).json({ message: "E-mail já cadastrado" });
+    }
     const cryptoPass = await bcrypt.hash(password, saltRounds);
     await connection
       .promise()
-      .query("INSERT INTO users (username, email, password, name, surname) VALUES (?, ?, ?, ?, ?)", [
-        username,
-        email,
-        cryptoPass,
-        name,
-        surname
-      ]);
+      .query(
+        "INSERT INTO users (username, email, password, name, surname) VALUES (?, ?, ?, ?, ?)",
+        [username, email, cryptoPass, name, surname]
+      );
     res.status(201).json({ message: "Usuário registrado com sucesso" });
   } catch (error) {
     console.log(error);
