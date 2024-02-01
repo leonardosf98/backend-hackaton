aabcebc232d569ae422af1445d590259343c4c50
Leonardo Souza <leonardosouza@TARS.local>
2024-02-01 12:23:13 -0300

    fix: missing table script

--- database-scripts.sql
+++ database-scripts.sql
@@ -14,8 +14,18 @@ CREATE TABLE `users` (
   UNIQUE KEY `username` (`user_username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- cadastro.projects definition
 
+CREATE TABLE `projects` (
+  `user_id` int NOT NULL,
+  `project_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `project_link` varchar(2048) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `project_id` int NOT NULL AUTO_INCREMENT,
+  `project_description` varchar(144) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`project_id`),
+  KEY `projects_users_FK` (`user_id`),
+  CONSTRAINT `projects_users_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 -- cadastro.tags definition
 
 CREATE TABLE `tags` (
