5aae9680af6bb2ad17caf65fb5b9348696fc89dd
Leonardo Souza <leonardosouza@TARS.local>
2024-02-29 11:40:10 -0300

    feat: update routes from index.js

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,27 +1,29 @@
 const connection = require('../database/connection');
-app.post('/addproject', async (req, res) => {
-  try {
-    const { userId, projectName, projectDescription, projectLink } = req.body;
-    const [result] = await connection
-      .promise()
-      .query(
-        'SELECT COUNT(user_id) AS userToCheck FROM cadastro.users WHERE user_id = ?',
-        [userId]
-      );
-    const [{ userToCheck }] = result;
-    if (userToCheck === 1) {
-      await connection
+module.exports = {
+  async addProject(req, res) {
+    try {
+      const { userId, projectName, projectDescription, projectLink } = req.body;
+      const [result] = await connection
         .promise()
         .query(
-          'INSERT INTO cadastro.projects (user_id, project_name, project_description, project_link) VALUES (?, ?, ?, ?)',
-          [userId, projectName, projectDescription, projectLink]
+          'SELECT COUNT(user_id) AS userToCheck FROM cadastro.users WHERE user_id = ?',
+          [userId]
         );
-      return res
-        .status(201)
-        .json({ message: 'Projeto cadastrado com sucesso!' });
+      const [{ userToCheck }] = result;
+      if (userToCheck === 1) {
+        await connection
+          .promise()
+          .query(
+            'INSERT INTO cadastro.projects (user_id, project_name, project_description, project_link) VALUES (?, ?, ?, ?)',
+            [userId, projectName, projectDescription, projectLink]
+          );
+        return res
+          .status(201)
+          .json({ message: 'Projeto cadastrado com sucesso!' });
+      }
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+    } catch (error) {
+      return res.status(500).json({ message: 'Erro ao cadastrar projeto' });
     }
-    return res.status(404).json({ message: 'Usuário não encontrado' });
-  } catch (error) {
-    return res.status(500).json({ message: 'Erro ao cadastrar projeto' });
-  }
-});
+  },
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,12 @@
 const { Router } = require('express');
 const userControllers = require('../controllers/userControllers');
+const projectControllers = require('../controllers/projectController');
 
 const routes = new Router();
 
 routes.post('/register', userControllers.register);
+routes.put('./edituserinfo', userControllers.editUserInfo);
+routes.get('/getuserinfo', userControllers.getUserInfo);
+
+routes.post('/projectadd', projectControllers.addProject);
 module.exports = routes;
